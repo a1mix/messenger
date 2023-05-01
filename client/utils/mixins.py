e841faf52164e64291d8a135f0614801bc8c81ca
a1mix <Aimix444@gmail.com>
2023-05-01 21:17:46 +0300

    step 6 done

--- client/utils/mixins.py
+++ client/utils/mixins.py
@@ -1,7 +1,8 @@
-from json import dumps
-from client.client_config import ENCODING
 from client.database.controller import ClientMessages
 from client.database.models import CBase
+
+from json import dumps, loads
+from client.client_config import ENCODING
 
 
 class DbInterfaceMixin:
@@ -19,6 +20,31 @@ class DbInterfaceMixin:
 
     def set_user_online(self, client_username):
         return self._cm.set_user_online(client_username)
+
+    def add_contact(self, client_username, contact_username):
+        return self._cm.add_contact(client_username, contact_username)
+
+    def del_contact(self, client_username, contact_username):
+        return self._cm.del_contact(client_username, contact_username)
+
+    def get_contacts(self, client_username):
+        return self._cm.get_contacts(client_username)
+
+    def get_all_clients(self):
+        return self._cm.get_all_clients()
+
+    def get_client_history(self, client_username):
+        return self._cm.get_client_history(client_username)
+
+    def get_client_messages(self, client_username):
+        return self._cm.get_client_messages(client_username)
+
+    def set_user_offline(self, client_username):
+        return self._cm.set_user_offline(client_username)
+
+    def get_user_status(self, client_username):
+        return self._cm.get_user_status(client_username)
+
 
 class ConvertMixin:
     def _dict_to_bytes(self, msg_dict):
@@ -49,8 +75,8 @@ class ConvertMixin:
 
             # Если там был словарь
             if isinstance(message, dict):
-                return message  # Возвращаем сообщение
+                return message              # Возвращаем сообщение
             else:
-                raise TypeError  # Нам прислали неверный тип
+                raise TypeError             # Нам прислали неверный тип
         else:
-            raise TypeError  # Передан неверный тип
+            raise TypeError                 # Передан неверный тип
